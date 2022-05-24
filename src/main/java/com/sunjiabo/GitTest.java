ebfb1751c59fe0fc0e85310983402a63de0fb3b5
ShmilyJ114 <106159138+ShmilyJ114@users.noreply.github.com>
2022-05-24 22:12:45 +0800

    commit

--- src/main/java/com/sunjiabo/GitTest.java
+++ src/main/java/com/sunjiabo/GitTest.java
@@ -2,7 +2,7 @@ package com.sunjiabo;
 
 public class GitTest {
     public static void main(String[] args) {
-        int i=0;
+        
         System.out.println("hello");
         System.out.println("hello1");
         System.out.println("hello1=2");
